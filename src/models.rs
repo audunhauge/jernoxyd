5ce676715adc833bb5d3572b3c667e56c278fab5
Audun Hauge <audun.hauge@gmail.com>
2019-12-30 11:46:18 +0100

    start of rust test

---
+++ src/models.rs
@@ -0,0 +1,6 @@
+#[derive(Queryable)]
+pub struct User {
+    pub id: i32,
+    pub firstname: String,
+    pub lastname: String,
+}
---
+++ src/schema.rs
@@ -0,0 +1,7 @@
+table! {
+    posts (id) {
+        id -> Integer,
+        firstname -> Text,
+        lastname -> Text,
+    }
+}
